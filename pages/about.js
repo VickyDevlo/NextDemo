3f063fb9be2f81042cf7ac00a9bed8619dc356f6
Pranav <pranavmagare2020@gmail.com>
2022-06-23 14:28:57 +0530

    Add

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,21 @@
 import styles from "../styles/Home.module.css";
-import Head from "next/head"; 
- 
+import Head from "next/head";
+import Image from "next/image";
 
- 
 export default function About() {
- 
   return (
     <div className={styles.container}>
       <Head>
         <title> Demo | about</title>
       </Head>
-      <img src="https://shopinindia.co/wp-content/uploads/2021/08/abt.png" alt="" />
-      <p className={styles.para}>We at Shopin India aim to provide the most convenient way to shop trendy jewellery online. We provide exclusive jewellery pieces and accessories at pocket friendly prices.  Your satisfaction is our priority and your trust is our responsibility. </p>
-     
+
+      <Image src="/../public/images/Logo.png" width={400} height={400} />
+      <p className={styles.para}>
+        We at Shopin India aim to provide the most convenient way to shop trendy
+        jewellery online. We provide exclusive jewellery pieces and accessories
+        at pocket friendly prices. Your satisfaction is our priority and your
+        trust is our responsibility.{" "}
+      </p>
     </div>
   );
 }
-
-
- 
